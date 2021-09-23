57c55ee9b1f0d2c5a80fdca7add16f247f3f1efc
Isabellanamutenda <namtendaisabella.com>
2021-09-23 12:08:51 +0300

    Make changes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,22 +14,22 @@ export default class Calculator extends Component {
         <span className="btn">AC</span>
         <span className="btn">+/-</span>
         <span className="btn">%</span>
-        <span className="btn sign-color">÷</span>
+        <span className="btn symbol-color">÷</span>
         <span className="btn">7</span>
         <span className="btn">8</span>
         <span className="btn">9</span>
-        <span className="btn sign-color">x</span>
+        <span className="btn symbol-color">x</span>
         <span className="btn">4</span>
         <span className="btn">5</span>
         <span className="btn">6</span>
-        <span className="btn sign-color">-</span>
+        <span className="btn symbol-color">-</span>
         <span className="btn">1</span>
         <span className="btn">2</span>
         <span className="btn">3</span>
-        <span className="btn sign-color">+</span>
+        <span className="btn symbol-color">+</span>
         <span className="zero">0</span>
         <span className="btn">.</span>
-        <span className="btn sign-color">=</span>
+        <span className="btn symbol-color">=</span>
       </div>
     );
   }
