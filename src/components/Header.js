0b345d49edec09af5593be92d95e83368e59247c
Isabellanamutenda <namtendaisabella.com>
2021-09-29 15:29:47 +0300

    Finished the calculator

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => (
+  <div className="header">
+    <div className="logo">
+      <h1>Math Magicians</h1>
+    </div>
+    <Navbar />
+  </div>
+);
+
+export default Header;
---
+++ src/pages/quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Quote = () => (
+  <h1 className="Quote" title="quote">
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    it&lsquo;s about understanding.--WIlliam Paul Thurston
+  </h1>
+);
+
+export default Quote;
